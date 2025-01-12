1a596224dd41037fdfe7be699bfde4e6d4d08880
Wisdom Onwuchekwa <144015437+WisdomPandovic@users.noreply.github.com>
2025-01-12 10:58:04 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -50,6 +50,10 @@ const UserSchema = new mongoose.Schema({
       default: 0,
     },
   },
+   following: {
+    type: Number,
+    default: 0,
+  },
   followers: {
     type: Number,
     default: 0,
